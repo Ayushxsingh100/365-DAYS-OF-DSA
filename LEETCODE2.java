349a34e16ad4ba33abf78a2ba21f98d49aa884c4
Ayush Kumar Singh <ayushxsingh100@outlook.com>
2026-02-03 23:48:02 +0530

    Refactor maxProfit method into Solution class
    Refactored the maxProfit method into a Solution class and improved variable handling for calculating maximum profit.

--- LEETCODE2.java
+++ LEETCODE2.java
@@ -1,23 +1,22 @@
-public class LEETCODE2 {
-    public static int maxProfit(int[] prices) {
+class Solution {
+    public int maxProfit(int[] prices) {
+        // giving the first element to min variable  ... 
+        int min = prices[0];
+        // initializing the profit with the 0
         int max_profit = 0;
-        int buy_price = Integer.MAX_VALUE;
-
-        for (int i = 0; i < prices.length; i++) {
-            if (prices[i] > buy_price) {
-                int profit = prices[i] - buy_price;
-                max_profit = Math.max(max_profit, profit);
-            } else {
-                buy_price = prices[i];
+        // traversing a LOOP for prices[]
+        for(int i =0 ; i< prices.length ; i++ ){
+            // if the value of the other elements is less than the first element of the 
+            // array , so we will change the value with the samllest element
+            if(prices[i] < min){
+                min = prices[i];
+            }
+            // when the value of the curr. element is more than that of the minimum element
+            // we will campare the max from the both one is profit and other is current profit
+            else{
+                max_profit = Math.max(max_profit , prices[i]-min);
             }
         }
-
-        return max_profit;
-    }
-
-    public static void main(String[] args) {
-        int[] prices = {7, 1, 5, 3, 6, 4}; // Example stock prices
-        int result = maxProfit(prices);
-        System.out.println("The maximum profit is: " + result);
+          return max_profit;
     }
 }
